2677ce6409f6d2000993d0f8e12ca5b82d2f91e8
My First Commit <Asher@Scotts-Mac-mini.attlocal.net>
2017-02-03 00:14:25 -0600

    no message

---
+++ DVDViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DVDViewController.swift
+//  DVDCollector
+//
+//  Created by Scott Asher on 2/3/17.
+//  Copyright © 2017 Scott Asher. All rights reserved.
+//
+
+import UIKit
+
+class DVDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
