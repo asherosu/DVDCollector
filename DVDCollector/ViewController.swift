267f38d217881ad9e368ad2436869b88d506876a
My First Commit <Asher@Scotts-Mac-mini.attlocal.net>
2017-02-02 22:19:22 -0600

    no message

---
+++ DVDCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DVDCollector
+//
+//  Created by Scott Asher on 2/2/17.
+//  Copyright © 2017 Scott Asher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
